08f69116ddf74be0613c4ea58ed4c59749d3bc18
agung <agung@rubyh.co>
2023-01-07 21:50:57 +0700

    test be fail

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -4,12 +4,13 @@ import {
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    getHealth
 } from "../controllers/UserController.js";
 
 const router = express.Router();
 
-router.get('/health', getUsers);
+router.get('/health', getHealth);
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
 router.post('/users', createUser);
