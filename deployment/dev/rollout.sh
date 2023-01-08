66cc37930ae063d30b6197fe5ea3110aeed6b564
agung <agung@rubyh.co>
2023-01-08 20:27:34 +0700

    fix replica

--- deployment/dev/rollout.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-
-if ! kubectl rollout status deployment  cilist-be-dev -n dev; then
-    kubectl rollout undo deployment  cilist-be-dev -n dev
-    kubectl rollout status deployment  cilist-be-dev -n dev
-    exit 1
-fi
